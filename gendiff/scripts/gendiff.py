cff07a1049dde5770021e04001a738a772b3b1c0
88ArtemSmirnov88 <artembanchik@mail.ru>
2023-02-21 18:28:45 +0300

    Arguments and options

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,8 @@ def main():
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    parser.add_argument('-f', '--format')
+    parser.add_argument('-f', '--format',
+                        help='set format of output')
     return parser.parse_args()
 
 
