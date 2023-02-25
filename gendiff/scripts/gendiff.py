d3f87c1750c2e31cb7d52dc3eea941d966dbe9d7
88ArtemSmirnov88 <artembanchik@mail.ru>
2023-02-25 13:06:25 +0300

    comparison flat files (json)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-
+from gendiff import generate_diff
 
 def main():
     parser = argparse.ArgumentParser(
@@ -10,7 +10,8 @@ def main():
     parser.add_argument("second_file")
     parser.add_argument('-f', '--format',
                         help='set format of output')
-    return parser.parse_args()
+    args = parser.parse_args()
+    return generate_diff(args.first_file, args.second_file)
 
 
 if __name__ == '__main__':
