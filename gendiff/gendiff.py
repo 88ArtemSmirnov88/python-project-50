0ad8837e41acba7b07cf75b03ae3d9af65f370a1
88ArtemSmirnov88 <artembanchik@mail.ru>
2023-03-13 17:08:51 +0300

    5 step

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -18,18 +18,18 @@ def generate_diff(first_file, second_file):
     data2 = check_boolean(dict(sorted(data2.items())))
     for key1 in data1:
         if key1 not in data2:
-            diff.append(f' - {key1}: {data1[key1]}')
+            diff.append(f'  - {key1}: {data1[key1]}')
         else:
             if data1[key1] == data2[key1]:
-                diff.append(f'   {key1}: {data1[key1]}')
+                diff.append(f'    {key1}: {data1[key1]}')
             else:
-                diff.append(f' - {key1}: {data1[key1]}')
-                diff.append(f' + {key1}: {data2[key1]}')
+                diff.append(f'  - {key1}: {data1[key1]}')
+                diff.append(f'  + {key1}: {data2[key1]}')
     for key2 in data2:
         if key2 not in data1:
-            diff.append(f' + {key2}: {data2[key2]}')
+            diff.append(f'  + {key2}: {data2[key2]}')
     diff = '\n'.join(diff)
-    return '{\n' + diff + '\n''}'
+    return '{\n' + diff + '\n''}\n'
 
 
 def check_boolean(data):
