c5ffe4930421caab0ccef95ad960d54e139805c4
88ArtemSmirnov88 <artembanchik@mail.ru>
2023-03-17 11:26:10 +0300

    Output in JSON

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -7,41 +7,8 @@ def generate_diff(first_file, second_file, format='stylish'):
     with open(first_file, 'r') as f1, open(second_file, 'r') as f2:
         file1 = f1.read()
         file2 = f2.read()
-        result = builder_tree(parse(file1), parse(file2))
-        return get_format(format, result)
-
-    # read files
-    # data1 = parse(first_file)
-    # data2 = parse(second_file)
-    #
-    # tree = builderTree(data1, data2)
-    # return formatter(tree)
-
-    # diff = []
-    # data1 = check_boolean(dict(sorted(data1.items())))
-    # data2 = check_boolean(dict(sorted(data2.items())))
-    # for key1 in data1:
-    #     if key1 not in data2:
-    #         diff.append(f'  - {key1}: {data1[key1]}')
-    #     else:
-    #         if data1[key1] == data2[key1]:
-    #             diff.append(f'    {key1}: {data1[key1]}')
-    #         else:
-    #             diff.append(f'  - {key1}: {data1[key1]}')
-    #             diff.append(f'  + {key1}: {data2[key1]}')
-    # for key2 in data2:
-    #     if key2 not in data1:
-    #         diff.append(f'  + {key2}: {data2[key2]}')
-    # diff = '\n'.join(diff)
-    # return '{\n' + diff + '\n''}\n'
-
-# def check_boolean(data):
-#     for key in data.keys():
-#         if data.get(key) is False:
-#             data[key] = 'false'
-#         if data.get(key) is True:
-#             data[key] = 'true'
-#     return data
+    result = builder_tree(parse(file1), parse(file2))
+    return get_format(format, result)
 
 
 def builder_tree(data1, data2):
