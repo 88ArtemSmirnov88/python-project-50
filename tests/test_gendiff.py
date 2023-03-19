34d96f49030ea8dba6a7b032ee9c7b8116ccf9ef
88ArtemSmirnov88 <artembanchik@mail.ru>
2023-03-19 22:10:47 +0300

    add space

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -9,3 +9,17 @@ def test_gendiff():
     result = open('tests/fixtures/result_json.txt').read()
     assert test_json == result
     assert test_yaml == result
+
+
+def test_gendiff_stylish_json():
+    test_json = generate_diff('tests/fixtures/file3.json',
+                              'tests/fixtures/file4.json')
+    result = open('tests/fixtures/result_stylish.txt').read()
+    assert test_json == result
+
+
+def test_gendiff_stylish_yaml():
+    test_yaml = generate_diff('tests/fixtures/file3.yaml',
+                              'tests/fixtures/file4.yaml')
+    result = open('tests/fixtures/result_stylish.txt').read()
+    assert test_yaml == result
