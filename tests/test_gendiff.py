5097f36ba68edb26e0320e861567540cf825dc0c
88ArtemSmirnov88 <artembanchik@mail.ru>
2023-04-04 21:49:48 +0300

    add test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -17,6 +17,7 @@ def test_gendiff(format):
     file_path1 = get_path(f'file.{format}')
     file_path2 = get_path(f'file2.{format}')
 
+    assert generate_diff(file_path1, file_path2) == read_file(get_path('result_stylish.txt'))
     assert generate_diff(file_path1, file_path2, 'stylish') == read_file(get_path('result_stylish.txt'))
     assert generate_diff(file_path1, file_path2, 'plain') == read_file(get_path('result_plain.txt'))
     assert generate_diff(file_path1, file_path2, 'json') == read_file(get_path('result_json.txt'))
